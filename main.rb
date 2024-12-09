aee2184ecac5cf3f259e738e873e5eb5b8ed8d9e
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-09 17:35:23 +0400

    Add Test

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'lib/list'
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
